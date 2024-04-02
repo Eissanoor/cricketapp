2e8100f9054f6299aa070e613ed6aae05f5d8841
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2024-04-02 12:09:35 +0500

    updateeddasd

--- router/admin.js
+++ router/admin.js
@@ -1534,8 +1534,71 @@ router.get("/get-upcoming-matches-for-user", async (req, res) =>
     });
   }
 });
+router.get("/get-live-matches/:adminId", async (req, res) =>
+{
+  try {
+    const adminId = req.params.adminId;
+    const matches = await MatchDetails.find({
+      admin: adminId,
+      matchStatus: 1,
+    }).populate("team1 team2 squad1 squad2");
 
+    if (!matches || matches.length === 0) {
+      return res.status(404).json({
+        status: 404,
+        success: false,
+        message: "No matches found for this admin ID with matchStatus 0",
+        data: null,
+      });
+    }
 
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: "Match details",
+      data: matches,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 500,
+      success: false,
+      message: "Internal server error",
+      data: null,
+    });
+  }
+});
+router.get("/get-live-matches-for-user", async (req, res) =>
+{
+  try {
+
+    const matches = await MatchDetails.find({ matchStatus: 1 }).populate("team1 team2 squad1 squad2");
+
+    if (!matches || matches.length === 0) {
+      return res.status(404).json({
+        status: 404,
+        success: false,
+        message: "No matches found for this admin ID with matchStatus 0",
+        data: null,
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: "Match details",
+      data: matches,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 500,
+      success: false,
+      message: "Internal server error",
+      data: null,
+    });
+  }
+});
 
 //socket.io
 
