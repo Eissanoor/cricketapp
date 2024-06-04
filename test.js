35d397faabc0957022ad628d96d21f2ef74b5bc5
Wasim <wasimxaman13@gmail.com>
2024-06-04 14:48:44 +0500

    refactor: Improve run rate calculation and usage in scorer.js
    Refactor the calculateNRR function in scorer.js to handle cases where oversFaced or oversBowled are falsy values, avoiding division by zero errors. Additionally, update the createPointsTable function to use the calculateNRR function with a single object argument instead of separate arguments. This improves code readability and reduces redundancy in the calculation of net run rate (NRR) for each team in the points table.

--- test.js
+++ test.js
@@ -1,34 +1,18 @@
-const mongoose = require("mongoose");
-const Team = require("./models/team"); // replace with the actual path to your Team model
-const addTeamRecentPerformance = require("./utils/scorer"); // replace with the actual path to your scorer.js file
+const calculateNRR = function calculateSingleMatchNRR(
+  runsScored,
+  oversFaced,
+  runsAgainst,
+  oversBowled
+) {
+  // Calculate run rate scored and run rate conceded
+  const runRateScored = runsScored / (oversFaced || 1);
+  const runRateConceded = runsAgainst / (oversBowled || 1);
 
-// Connect to your MongoDB database
-mongoose
-  .connect(
-    "mongodb://eissanoor:Eisa.123@ac-fpqi8sr-shard-00-00.olvd0r5.mongodb.net:27017,ac-fpqi8sr-shard-00-01.olvd0r5.mongodb.net:27017,ac-fpqi8sr-shard-00-02.olvd0r5.mongodb.net:27017/cricket?replicaSet=atlas-zbl1w1-shard-0&ssl=true&authSource=admin",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
+  // Calculate net run rate
+  const netRunRate = runRateScored - runRateConceded;
 
-async function testAddTeamRecentPerformance() {
-  const team1 = "65fa923da2e3a19ac328a2c2"; // replace with actual team1 id
-  const team2 = "6607d7e5b4ab4dfe96c4c311"; // replace with actual team2 id
-  const matchId = "6607d7e5b4ab4dfe96c4c311"; // replace with actual match id
-  const wins = true;
-  const wonByRuns = 10;
+  // Return the NRR rounded to two decimal places
+  return netRunRate.toFixed(2);
+};
 
-  try {
-    await addTeamRecentPerformance(team1, team2, matchId, wins, wonByRuns);
-    console.log("Team recent performance added successfully");
-  } catch (error) {
-    console.error("Failed to add team recent performance", error);
-  }
-
-  mongoose.connection.close();
-}
-
-testAddTeamRecentPerformance();
+console.log(calculateNRR(6, 1, 4, 1));
