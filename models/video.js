7c888a3ebccc49bf5162980f0b2d68050259e11a
Wasim <wasimxaman13@gmail.com>
2024-06-05 08:00:29 +0500

    refactor: Update ManuImage handling in postAddPlayer endpoint

--- models/video.js
+++ models/video.js
@@ -1,29 +1,34 @@
 const mongoose = require("mongoose");
 
-const videoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  videoUrl: {
-    type: String,
-    required: true,
-  },
-  thumbnail: {
-    type: String,
-    required: true,
-  },
-  viewers: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "admin",
+const videoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    description: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    viewers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "admin",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Video = mongoose.model("Video", videoSchema);
 
