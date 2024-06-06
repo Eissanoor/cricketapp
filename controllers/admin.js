8844136bdf38c24670aaf3138f753edbbdcb0759
Wasim <wasimxaman13@gmail.com>
2024-06-06 14:41:31 +0500

    feat: Update postAddPlayer endpoint to handle image upload with Cloudinary

--- controllers/admin.js
+++ controllers/admin.js
@@ -659,8 +659,8 @@ exports.postAddPlayer = async (req, res, next) => {
       ? admins.map((id) => mongoose.Types.ObjectId(id))
       : [];
 
-    const imageUrl = req.file ? req.file.path : null;
-    const publicId = req.file ? req.file.filename : null;
+    let imageUrl = null;
+    let publicId = null;
 
     if (req.file) {
       const result = await cloudinary.uploader.upload(req.file.path);
